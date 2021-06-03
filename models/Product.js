4ae17e88cc5cfc970fb6691688e91672117a069d
specOper99 <mohammadnawfal197@gmail.com>
2021-06-03 17:17:43 +0300

    first commit

---
+++ models/Product.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  imagePath: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  department: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    trim: true,
+  },
+  size: {
+    type: String,
+    trim: true,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Product = mongoose.model('Product-Mustafa', productSchema);
+module.exports = Product;
